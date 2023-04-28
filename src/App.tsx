8d09c540bb20ebca0e70a47937aa9d2de3567eca
Chris <chrisalexander0617@gmail.com>
2023-04-27 20:48:47 -0400

    updated it

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,34 @@ import { auth } from "./config/firebase";
 import routes from "./config/routes";
 import Center from "./components/utils/Center";
 import AuthChecker from "./components/auth/AuthChecker";
-import { getUserByGoogleUID } from './api/UserAccountService'
+import { getUserByGoogleUID, createNewUserWithGoogleUID } from './api/UserAccountService'
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    auth.onAuthStateChanged(async (user) => {
       if (user) {
         console.info("User detected.");
         console.log('Current user ID', user.uid) // store this in the database
 
-        const result = getUserByGoogleUID(user.uid)
-        console.log('Result', result)
+        try {
+          const result = await getUserByGoogleUID(user.uid)
+          console.log('Result', result)
+
+        } catch (err) {
+          console.log('Result', err)
+          console.log('No user was found with that ID')
+
+          try {
+            const result = await createNewUserWithGoogleUID(user.uid)
+            console.log('here is the result', result)
+          } catch (err) {
+            console.log('Error', err)
+          }
+
+          setLoading(false);
+        }
 
       } else {
         console.info("No user detected");
--- src/api/UserAccountService.tsx
+++ src/api/UserAccountService.tsx
@@ -1,16 +1,51 @@
 import userEvent from '@testing-library/user-event'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { IUser } from '../types'
 
 export const getUserByGoogleUID = async (uid: string): Promise<IUser> => {
-  const apiURL = `http://localhost:4001/users/google/${uid}`
+  const apiURL = `http://localhost:4001/users/google/${uid}`;
 
   try {
-    const response = await axios.get(apiURL)
-    const userData = response.data as IUser
-    return userData
-
-  } catch (err) {
-    throw err
+    const response = await axios.get<IUser>(apiURL);
+    return response.data;
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      const axiosError = err as AxiosError;
+      if (axiosError.response?.status === 404) {
+        throw new Error(`User with Google UID ${uid} not found`);
+      } else {
+        console.log(axiosError.response)
+        throw new Error(`Failed to get user with Google UID ${uid}: ${axiosError.message}`);
+      }
+    } else {
+      throw new Error(`Failed to get user with Google UID ${uid}: ${String(err)}`);
+    }
   }
-}
+};
+
+export const createNewUserWithGoogleUID = async (uid: string): Promise<IUser> => {
+  const apiURL = `http://localhost:4001/users/`;
+  const requestData = {
+    googleUID: uid,
+    title: "Software Engineer IV",
+    firstName: "Christopher",
+    lastName: "Clemmons"
+  };
+
+  try {
+    const response = await axios.post<IUser>(apiURL, requestData);
+    return response.data;
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      const axiosError = err as AxiosError;
+      if (axiosError.response?.status === 404) {
+        throw new Error(`User with Google UID ${uid} not found`);
+      } else {
+        console.log(axiosError.response)
+        throw new Error(`Failed to create user with Google UID ${uid}: ${axiosError.message}`);
+      }
+    } else {
+      throw new Error(`Failed to create user with Google UID ${uid}: ${String(err)}`);
+    }
+  }
+};
